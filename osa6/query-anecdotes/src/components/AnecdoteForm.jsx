507e51bf42c44984da2f8d770eb4b86b9adda54f
kxelina <elina88.k@gmail.com>
2024-09-18 18:55:46 +0300

    osa6:6.22

---
+++ osa6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const AnecdoteForm = ({ addAnecdote }) => {
+
+  const onCreate = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    console.log('new anecdote')
+    addAnecdote({content, votes: 0})
+}
+
+  return (
+    <div>
+      <h3>create new</h3>
+      <form onSubmit={onCreate}>
+        <input name='anecdote' />
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default AnecdoteForm
